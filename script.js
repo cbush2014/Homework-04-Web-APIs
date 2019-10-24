edd5ec904dbef09d42e86573ca3a0a14292e108e
cbush19 <cbush5400@gmail.com>
2019-10-23 20:55:36 -0400

    added code to populate quiz questions

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
 var timeEl = document.getElementById("timer");
-var secondsLeft = 3;
+var secondsLeft = 13;
+
+var qTitle = document.getElementById("qTitle");
+var c1El = document.getElementById("btnA1");
+var c2El = document.getElementById("btnA2");
+var c3El = document.getElementById("btnA3");
+var c4El = document.getElementById("btnA4");
+var ansEl = document.getElementById("qAnswer");
+
+function loadQuestion(){
+
+  var title = questions[0].title;
+  var choice1 = questions[0].choices[0];
+  var choice2 = questions[0].choices[1];
+  var choice3 = questions[0].choices[2];
+  var choice4 = questions[0].choices[3];
+  var answer =  questions[0].answer;
+  alert("|"+title+ " / "+answer+"|");
+  // console.log(questions[0].title);
+  // console.log(questions[0].choices[0]);
+  // console.log(questions[0].choices[1]);
+  // console.log(questions[0].choices[2]);
+  // console.log(questions[0].choices[3]);
+  // console.log(questions[0].answer);
+  //  qTitle.textContent = questions[0].title;
+  // c1El.textContent = questions[0].choices[0];
+  // c2El.textContent = questions[0].choices[1];
+  // c3El.textContent = questions[0].choices[2];
+  // c4El.textContent = questions[0].choices[3];
+  qTitle.textContent = title;
+  c1El.textContent = choice1;
+  c2El.textContent = choice2;
+  c3El.textContent = choice3;
+  c4El.textContent = choice4;
+  qAnswer.textContent= answer;
+console.log(questions);
+}
+
+
 
 function setTime() {
   var timerInterval = setInterval(function() {
     secondsLeft--;
-    timeEl.textContent = "Time: "+ secondsLeft;
+    // timeEl.textContent = "Time: "+ secondsLeft;
+    timeEl.textContent = "Time: "+ secondsLeft.toString().padStart(2, '0');
 
     if(secondsLeft === 0) {
       console.log("--- time has run out! --- clear timer")
@@ -16,10 +55,13 @@ function setTime() {
   }, 1000);
 }
 
-function takeQuiz() {
-//   timeEl.textContent = "Time: 0"+ ;
 
-  //  start timer 
+function takeQuiz() {
+
+    //populate 1st quiz
+    loadQuestion();
+
+    //  start timer 
   setTime();
 
 }
