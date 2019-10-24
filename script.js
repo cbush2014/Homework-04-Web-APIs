c5cb4cec91cf832d92702d620e5f809289c87bf3
cbush19 <cbush5400@gmail.com>
2019-10-24 00:02:22 -0400

    update many things

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var timeEl = document.getElementById("timer");
-var secondsLeft = 13;
+var secondsLeft = 75;
 
 var qTitle = document.getElementById("qTitle");
 var c1El = document.getElementById("btnA1");
@@ -8,49 +8,40 @@ var c3El = document.getElementById("btnA3");
 var c4El = document.getElementById("btnA4");
 var ansEl = document.getElementById("qAnswer");
 
-function loadQuestion(){
+function loadQuestion(idx) {
 
-  var title = questions[0].title;
-  var choice1 = questions[0].choices[0];
-  var choice2 = questions[0].choices[1];
-  var choice3 = questions[0].choices[2];
-  var choice4 = questions[0].choices[3];
-  var answer =  questions[0].answer;
-  alert("|"+title+ " / "+answer+"|");
-  // console.log(questions[0].title);
-  // console.log(questions[0].choices[0]);
-  // console.log(questions[0].choices[1]);
-  // console.log(questions[0].choices[2]);
-  // console.log(questions[0].choices[3]);
-  // console.log(questions[0].answer);
-  //  qTitle.textContent = questions[0].title;
-  // c1El.textContent = questions[0].choices[0];
-  // c2El.textContent = questions[0].choices[1];
-  // c3El.textContent = questions[0].choices[2];
-  // c4El.textContent = questions[0].choices[3];
+  var title = questions[idx].title;
+  var choice1 = questions[idx].choices[0];
+  var choice2 = questions[idx].choices[1];
+  var choice3 = questions[idx].choices[2];
+  var choice4 = questions[idx].choices[3];
+  var answer = questions[idx].answer;
+
   qTitle.textContent = title;
   c1El.textContent = choice1;
   c2El.textContent = choice2;
   c3El.textContent = choice3;
   c4El.textContent = choice4;
-  qAnswer.textContent= answer;
-console.log(questions);
+  qAnswer.textContent = answer;
+
 }
 
 
-
 function setTime() {
-  var timerInterval = setInterval(function() {
+  var timerInterval = setInterval(function () {
     secondsLeft--;
     // timeEl.textContent = "Time: "+ secondsLeft;
-    timeEl.textContent = "Time: "+ secondsLeft.toString().padStart(2, '0');
+    timeEl.textContent = "Time: " + secondsLeft.toString().padStart(2, '0');
 
-    if(secondsLeft === 0) {
+    if (secondsLeft === 0) {
       console.log("--- time has run out! --- clear timer")
       clearInterval(timerInterval);
-
       toggleSection();
     }
+
+    // This section of code is for taking the quiz
+
+
 
   }, 1000);
 }
@@ -58,26 +49,27 @@ function setTime() {
 
 function takeQuiz() {
 
-    //populate 1st quiz
-    loadQuestion();
+  //populate 1st quiz question
+  loadQuestion(0);
+  toggleSection();
 
-    //  start timer 
+  //  start timer 
   setTime();
 
 }
- 
-function toggleSection(){
 
-  var startEl= document.getElementById("startQuiz");
+function toggleSection() {
+
+  var startEl = document.getElementById("startQuiz");
   var takeEl = document.getElementById("takeQuiz");
 
   console.log(startEl);
-  if ( startEl.style.display === "none") {
-        startEl.style.display = "block";
-        takeEl.style.display= "none";
+  if (startEl.style.display === "none") {
+    startEl.style.display = "block";
+    takeEl.style.display = "none";
   } else {
-        startEl.style.display = "none";
-        takeEl.style.display= "block";
+    startEl.style.display = "none";
+    takeEl.style.display = "block";
   }
 }
 
@@ -89,14 +81,11 @@ document.querySelector("#startBtn").onclick = function (event) {
 
   console.log('Start button fired');
 
-  if(event === null){
+  if (event === null) {
     return;
   }
-    console.log( "invoke takeQuiz function");
 
-    toggleSection();
-
-    takeQuiz();
+  takeQuiz();
 }
 
 
