6a4794ec2ddca0c40f5144c3e40e5ce165f80a3e
cbush2019 <cbush5400@gmail.com>
2019-10-25 06:32:51 -0400

    fixed somethings, broke some others

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var timeEl = document.getElementById("timer");
 var secondsLeft = 70;
+var timerInterval;
 
 var qTitle = document.getElementById("Question");
 var c1El = document.getElementById("btnA1");
@@ -11,9 +12,12 @@ var sScore = document.getElementById("finalScore");
 var sMain = document.getElementById("startQuiz");
 var msgEl = document.getElementById("result");
 var msgDone = document.getElementById("msgQuizDone");
+var msgScoreEl = document.getElementById("msgScore");
 
 var idxQuestion = 0;  // first question starts at 0
 var blnCorrect = false;
+var numCorrectAnswers = 0;
+var numTotalQuestions = 0;
 
 sTakeQ.addEventListener("click", function (event) {
   var element = event.target;
@@ -24,10 +28,12 @@ sTakeQ.addEventListener("click", function (event) {
 
     msgEl.textContent = element.getAttribute("data-answered");
     msgEl.style.color = "red";
-
+    console.log(msgEl);
     if (element.getAttribute("data-answered") === "Correct") {
       blnCorrect = true;
       msgEl.style.color = "green";
+      numCorrectAnswers++;
+
     } else {
       // incorrect answer, penalize them 15 second
       secondsLeft -= 15;
@@ -48,6 +54,7 @@ function loadQuestion() {
     return;
   }
   var correctAnswer = questions[idxQuestion].answer;
+  numTotalQuestions++;
 
   //we have a global questions array already in memory, and the current index
   qTitle.textContent = questions[idxQuestion].title;
@@ -58,7 +65,6 @@ function loadQuestion() {
       idxCorrect = i;
     }
   }
-
   c1El.textContent = questions[idxQuestion].choices[0];
   c2El.textContent = questions[idxQuestion].choices[1];
   c3El.textContent = questions[idxQuestion].choices[2];
@@ -89,11 +95,11 @@ function loadQuestion() {
 
 
 function setTime() {
-  var timerInterval = setInterval(function () {
+  timerInterval = setInterval(function () {
     secondsLeft--;
 
-    if (secondsLeft < 0) {
-      secondsLeft === 0;
+    if (!secondsLeft > 0) {
+      secondsLeft = 0;
     }
     // timeEl.textContent = "Time: "+ secondsLeft;
     timeEl.textContent = "Time: " + secondsLeft.toString().padStart(2, '0');
@@ -102,22 +108,41 @@ function setTime() {
   }, 1000);
 }
 
-function checkTimeRemaining(){
-    console.log("you have "+ secondsLeft +" on the timer");
+function checkTimeRemaining() {
+
   if (secondsLeft <= 0) {
     // console.log("--- time has run out! --- clear timer")
+    setTimeout(function () {
+      document.getElementById("msgQuizDone").textContent = "Your Time is Up!";
+      document.getElementById("msgQuizDone").style.color = "red";
+    }, 1000);
+
     clearInterval(timerInterval);
     showFinalScore();
   }
 }
 
-function showFinalScore(){
-    // hide the main section and the quiz section
-    msgDone.element.textContent = "All done!";
-    alert("showFinalScore");
-     sTakeQ.classList.add("d-none")
-    // sMain.classList.add("d-none");
-     sScore.classList.remove("d-none");
+function showFinalScore() {
+  // hide the main section and the quiz section
+  sTakeQ.classList.add("d-none")
+  // sMain.classList.add("d-none");
+  sScore.classList.remove("d-none");
+  document.getElementById("msgQuizDone").textContent = "All done!";
+  if (!secondsLeft > 0) {
+    secondsLeft = 0;
+  }
+  alert(secondsLeft);
+  if (numCorrectAnswers > 0) {
+    document.getElementById("msgScore").textContent = "Your final score is " + (numCorrectAnswers / numTotalQuestions) + (0.2 * secondsLeft);
+  } else {
+    document.getElementById("msgScore").textContent = "Your final score is 0";
+  }
+
+  // document.getElementById("msgScore").textContent = "Your final score is "+ (idxCorrect/numTotalQuestions)+(0.2*secondsLeft);
+  // completely bogus waste of hours, the 2 lines above this work, and the 2 commented lines below do Not work
+  // msgDone.getElementById("msgQuizDone").textContent = "All done!";
+  // msgScoreEl.getElementById("msgScore").textContent = "Your final score is "+ (idxCorrect/numTotalQuestions)+(0.2*secondsLeft);
+
 }
 
 function takeQuiz() {
