08890fe08f3cd8cccac5ddebfb172f686a7b1e62
cbush19 <cbush5400@gmail.com>
2019-11-07 15:53:26 -0500

    update questions to add more

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # Homework-04-Web-APIs
-timer-based quiz application that stores high scores client-side
+A timer-based quiz application that stores high scores client-side
+
+## Table of contents
+* [General info](#general-info)
+* [Screenshots](#screenshots)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [Features](#features)
+
+## General info
+Unit 04 Web APIs Homework: Code Quiz
+In this homework assignment, your challenge is to build a code quiz with multiple-choice questions.
+
+Add more general information about project. What the purpose of the project is? Motivation?
+
+## Screenshots
+![Example screenshot](.img/SS-main.jpg)
+![Example screenshot](./img/SS-highscores.jpg)
+![Example screenshot](./img/SS-msg-score-zero.jpg)
+![Example screenshot](./img/SS-msg-enter-initials.jpg)
+![Example screenshot](./img/screenshot.png)
+
+![Example screenshot](./img/screenshot.png)
+## Technologies
+* Tech 1 - version 1.0
+* Tech 2 - version 2.0
+* Tech 3 - version 3.0
+
+## Setup
+Describe how to install / setup your local environement / add link to demo version.
+
+## Code Examples
+Show examples of usage:
+`put-your-code-here`
+
+## Features
+List of features ready and TODOs for future development
+* Green/Red colored answer indicator displayed for correct or incorrect answers
+* High scores are saved to local storage and displayed in descending order on another window
+* Answers are shuffled each time you take the quiz
+
+To-do list:
+* Create maintenance screen so user can add/edit quiz questions
+* Add audio clips to play on correct/incorrect answers
+
+## Status
+Project is: _in progress_, _finished_, _no longer continue_ and why?
--- questions.js
+++ questions.js
@@ -1,43 +1,53 @@
 var questions = [
     {
-      title: "1Commonly used data types DO NOT include:",
+      title: "Commonly used data types DO NOT include:",
       choices: ["strings", "booleans", "alerts", "numbers"],
       answer: "alerts"
     },
     {
-      title: "2The condition in an if / else statement is enclosed within ____.",
+      title: "The condition in an if / else statement is enclosed within ____.",
       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
       answer: "parentheses"
     },
     {
-      title: "3Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+      title: "Repeats until a specific condition becomes false:",
+      choices: ["What is the DOM?", "Do While Loop", "For Loop Example", "What is a variable?"],
+      answer:  "For Loop Example"
     },
     {
-      title: "4The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      title: "Removes the first element of an array and returns that element",
+      choices: ["Dynamic Typing", "shift()", "Function", "push()"],
+      answer: "shift()"
     },
     {
-      title: "5Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+      title: "Returns the first Element within the document that matches the specified selector, or group of selectors, or null, if no matches are found.",
+      choices: ["What is variable 'Hoisting'", "document.querySelectorAll()", "Multi Paradigm Language", "document.querySelector()"],
+      answer: "document.querySelector()"
     },
     {
-      title: "6The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      title: "Can read and alter the elements on a webpage",
+      choices: ["addEventListener", "For Loop Example", "intervalid", ".innerHTML"],
+      answer: ".innerHTML"
     },
     {
-      title: "7Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+      title: "True or False",
+      choices: ["What kind of value does Boolean take", "What will an undeclared variable return?", "What are the Strings within Datatypes?", "What are the two ways to add JS to your site?"],
+      answer: "What kind of value does Boolean take"
+    }, 
+    {
+      title: "Integer: Whole Number (-1,450) Float: Decimal Point (0.1, 12.454)",
+      choices: ["What happens if you call the 'document.write' function after the document has fully loaded?", "What are the two types of Numbers within Datatypes?", "What are the two ways to add JS to your site?","What type of language is JavaScript?"],
+      answer: ""
     },
     {
-      title: "8The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      title: "Representation of HTML Page...",
+      choices: ["What is the DOM?", "What is DOM?", "What is a variable", "Math.random()"],
+      answer: "What is DOM?"
+    },
+    {
+      title: "Adds new elements to the beginning of an array, and returns the new length",
+      choices: ["unshift()", "Operators", ".innerHTML", "Do While Loop"],
+      answer: ""
     },
     ///etc.
   ];
